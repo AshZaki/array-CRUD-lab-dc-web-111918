bc8dd37dc7be23471ce605fc92e91a0a450628fd
Ashley  Zaki <ahnahn.un@gmail.com>
2018-10-16 19:26:48 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -12,7 +12,7 @@ def add_element_to_end_of_array(array, element)
 end
 
 def add_element_to_start_of_array(array, element)
-  favorite_color = ["yello", "pink", "green", "blue"]
+  
   favorite_color.unshift(element)
 end
 
