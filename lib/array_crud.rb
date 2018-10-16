e067efa357e0e4d2db783ff0770d99e546bf5e3a
Ashley  Zaki <ahnahn.un@gmail.com>
2018-10-16 19:29:52 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -17,7 +17,7 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
-  blue = favorite_color.pop(array)
+  array = favorite_color.pop(array)
 end
 
 def remove_element_from_start_of_array(array)
