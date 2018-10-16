475872c6a400cbbacd11e10a1a1eea424269545c
Ashley  Zaki <ahnahn.un@gmail.com>
2018-10-16 19:19:26 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,8 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  favorite_color = ["yello", "pink", "green", "blue"]
+  favorite_color << "grey"
 end
 
 def add_element_to_start_of_array(array, element)
